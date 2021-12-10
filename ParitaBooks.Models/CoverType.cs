446638eb759486022fa91f24fd0a69959625f91d
Parita2910 <91142418+Parita2910@users.noreply.github.com>
2021-12-09 23:02:15 -0500

    Add files via upload

--- ParitaBooks.Models/CoverType.cs
+++ ParitaBooks.Models/CoverType.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace ParitaBooks.Models
 {
-    public class Category
+    public class CoverType
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name = "Category Name")]
         [Required]
+        [Display(Name = "Cover Type")]
         [MaxLength(50)]
         public string Name { get; set; }
     }
